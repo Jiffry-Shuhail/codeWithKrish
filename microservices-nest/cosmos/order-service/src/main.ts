dc16b3eec230dbee316d7979e298fffcf47d70be
Shuhail jiffry <shuhail1994@gmail.com>
2025-02-28 16:02:46 +0530

    Cross Orgin Changes

--- microservices-nest/cosmos/order-service/src/main.ts
+++ microservices-nest/cosmos/order-service/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Its Asscess granted CrossOrigin 
+  app.enableCors({origin:'http://localhost:3001'});
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
